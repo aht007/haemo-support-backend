f2e5683cac1f94aaac9d67f0a1df1ea47db1eab9
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-08-05 17:50:21 +0500

    cors issue resolved for jwt, healthProfile get api modified

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('auth/register', RegisterAPI.as_view()),
     path('auth/login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/user', UserAPI.as_view()),
     path('auth/users', UsersList.as_view()),
 ]
