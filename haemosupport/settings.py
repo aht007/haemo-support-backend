cfb810b04e676ff8235adccf0aeb900e802d43c4
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-09-02 09:44:34 +0500

    token rotation turned off

--- haemosupport/settings.py
+++ haemosupport/settings.py
@@ -177,8 +177,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': False,
 }
