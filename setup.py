f4b4185fef42177709c5c1ecc528c6ac2ab0b24a
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-09-13 19:22:23 +0500

    workflow for tox added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='haemosupport',
+    version='1.0',
+    description='Blood Donation App',
+    author='Aht-007',
+    author_email='ahthassan74@gmail.com',
+    packages=['haemosupport']
+)
