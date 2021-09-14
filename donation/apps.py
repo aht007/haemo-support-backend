011bd0167a17660093cdab6a16211662d6a07f65
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-09-14 12:43:05 +0500

    bug resolved

--- donation/apps.py
+++ donation/apps.py
@@ -6,7 +6,10 @@ from django.apps import AppConfig
 
 class DonationConfig(AppConfig):
     """
-    Donation App Configuration
+    Donation App configuration
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'donation'
+
+    def ready(self):
+        import donation.signals
