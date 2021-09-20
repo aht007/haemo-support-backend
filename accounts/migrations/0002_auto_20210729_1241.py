3ede29a029f8cb762d7d526393b3a69af7849fac
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-09-20 09:41:28 +0500

    ran migrations

--- accounts/migrations/0002_auto_20210729_1241.py
+++ accounts/migrations/0002_auto_20210729_1241.py
@@ -14,11 +14,13 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='my_user',
             name='blood_group',
-            field=models.CharField(blank=True, choices=[('A+', 'Apositive'), ('A-', 'Anegative'), ('B+', 'Bpositive'), ('B-', 'Bnegative'), ('O+', 'Opositive'), ('O-', 'Onegative'), ('AB+', 'Abpositive'), ('AB-', 'Abnegative')], max_length=3),
+            field=models.CharField(blank=True, choices=[('A+', 'Apositive'), ('A-', 'Anegative'), ('B+', 'Bpositive'), (
+                'B-', 'Bnegative'), ('O+', 'Opositive'), ('O-', 'Onegative'), ('AB+', 'Abpositive'), ('AB-', 'Abnegative')], max_length=3),
         ),
         migrations.AlterField(
             model_name='my_user',
             name='phone_number',
-            field=models.CharField(max_length=17, validators=[django.core.validators.RegexValidator(message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.", regex='^\\+?1?\\d{9,15}$')]),
+            field=models.CharField(max_length=17, validators=[django.core.validators.RegexValidator(
+                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.", regex='^\\+?1?\\d{9,15}$')]),
         ),
     ]
